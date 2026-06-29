1beba62096baa0d1c970fa73af9d6917500f86c8
Yukith M Joseph <179663742+Zynx095@users.noreply.github.com>
2026-06-29 13:08:32 +0530

    fixed transporter issue for mail

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -9,16 +9,18 @@ console.log(
 console.log(
     process.env.EMAIL_PASS
 );
-const transporter =
-    nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user:
-                process.env.EMAIL_USER,
-            pass:
-                process.env.EMAIL_PASS
-        }
-    });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
 export const sendInvoiceEmail =
     async (
