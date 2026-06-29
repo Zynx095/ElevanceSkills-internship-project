a019ca20c509f771b3e45a5e6cb0a2f746dbf5ee
Yukith M Joseph <179663742+Zynx095@users.noreply.github.com>
2026-06-29 13:51:53 +0530

    email service changes

--- server/utils/resend.js
+++ server/utils/resend.js
@@ -1,33 +1,7 @@
 import { Resend } from "resend";
 import dotenv from "dotenv";
-dot.config();
+
+dotenv.config();
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const FROM = "Yukith Hub <noreply@yukithjoseph.me>";
-
-export const sendEmail = async ({
-  to,
-  subject,
-  html,
-}) => {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: FROM,
-      to,
-      subject,
-      html,
-    });
-
-    if (error) {
-      console.error("Resend Error:", error);
-      throw new Error(error.message);
-    }
-
-    console.log("Email Sent:", data);
-
-    return data;
-  } catch (err) {
-    console.error("Email Service Error:", err);
-    throw err;
-  }
-};
+export default resend;
