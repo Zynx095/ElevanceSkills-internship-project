a994d5030fde0c0176e493d1ef706edbce17a042
Yukith M Joseph <179663742+Zynx095@users.noreply.github.com>
2026-06-30 10:10:49 +0530

    Checking for subscription errors

--- server/utils/razorpay.js
+++ server/utils/razorpay.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const razorpay =
+console.log("KEY:", process.env.RAZORPAY_KEY_ID);
+console.log("SECRET EXISTS:", !!process.env.RAZORPAY_SECRET);
   new Razorpay({
     key_id:
       process.env.RAZORPAY_KEY_ID,
